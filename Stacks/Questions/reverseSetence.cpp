9925b23c274a88ae401473b23a6ca39b517f5e54
Vishal Panwar <119125256+vishallpanwar@users.noreply.github.com>
2023-02-12 21:31:38 +0530

    Add files via upload

---
+++ Stacks/Questions/reverseSetence.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+
+void reverse(string s){
+
+    stack<string> st;
+
+    for(int i=0;i<s.length();i++){
+
+        string word="";
+
+        while(s[i] != ' ' && i<s.length()){
+        word+=s[i];
+        i++;
+        }
+
+        st.push(word);
+    }
+
+    while(!st.empty()){
+
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+
+    cout<<endl;
+
+}
+
+
+int main(){
+
+    string s="My name is Vishal Panwar";
+    reverse(s);
+    return 0;
+}
