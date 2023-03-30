ee93cd800a5e768ad4500dee5b6684d5d8b4f02f
Vishal Panwar <119125256+vishallpanwar@users.noreply.github.com>
2023-03-30 13:01:14 +0530

    Add files via upload

---
+++ Recursion/Questions/factorial.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int fact(int n){
+
+    // Base case
+    if(n==0)
+    return 1;
+
+    return n * fact(n-1);
+}
+
+int main(){
+
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+
+    cout<<fact(n);
+    return 0;
+
+}
